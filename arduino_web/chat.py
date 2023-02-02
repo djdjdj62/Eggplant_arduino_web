9349ca45e061deaa99c77a0f77b2f8ef62aed691
unknown <dj0976589080@gmail.com>
2023-02-02 17:32:39 +0800

    12

--- arduino_web/chat.py
+++ arduino_web/chat.py
@@ -1,21 +1,21 @@
 import os
 import openai
 
-def chat_gpt(chat_data):
-  openai.api_key = "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"
+openai.api_key = "sk-"+"yfCjXJoJXgjUX2LqMB5MT3BlbkFJh5Y81yB0Rph7qKUhJ9G9"
 
-  start_sequence = "\nAI:"
-  restart_sequence = "\nHuman: "
+aa = "聊天講一個笑話"
 
-  response = openai.Completion.create(
-    model="text-davinci-003",
-    prompt="\nHuman:"+chat_data+"\nAI:",
-    temperature=0.9,
-    max_tokens=100,
-    top_p=1,
-    frequency_penalty=0,
-    presence_penalty=0.6,
-    stop=[" Human:", " AI:"]
-  )
-  #print(response["choices"][0]["text"])
-  return response["choices"][0]["text"]
+start_sequence = "\nAI:"
+restart_sequence = "\nHuman: "
+
+response = openai.Completion.create(
+  model="text-davinci-003",
+  prompt="\nHuman:"+aa[2:]+"\nAI:",
+  temperature=0.9,
+  max_tokens=100,
+  top_p=1,
+  frequency_penalty=0,
+  presence_penalty=0.6,
+  stop=[" Human:", " AI:"]
+)
+print(response["choices"][0]["text"])
